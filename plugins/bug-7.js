d881bcdedd84d4b9d46b9d8f44085620877bed83
Razzdedsecc <128728458+Razzdedsecc@users.noreply.github.com>
2023-06-15 20:24:07 +0800

    Razz

---
+++ plugins/bug-7.js
@@ -0,0 +1,213 @@
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+    let handler = async (m, { conn, text }) => {
+    if (!text) throw 'Masukan Nomor....'
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     await sleep(20)
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     await sleep(20)
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+    conn.sendMessage(text + '@s.whatsapp.net', { 
+    text: 'POWERED BY 𝔻𝕖𝕕𝕊𝕖𝕔 ~ 𝔹𝕠𝕥𝕫', 
+    templateButtons: [
+    { callButton: { displayText: `Number Phone Owner`, phoneNumber: `owner`}},
+    { urlButton: { displayText: `OWNER`, url: 'https://wa.me/6289668942278'}},
+    { urlButton: { displayText: `ID GORUP`, url: 'https://chat.whatsapp.com/LGom3xGkji8BeNBQxNAmjL'}},
+    { quickReplyButton: { displayText: `ʀᴜʟᴇs`, id: `rules`}},
+    { quickReplyButton: { displayText: `ɪɴғᴏ ʙᴏᴛᴢ`, id: `x`}},
+    { quickReplyButton: { displayText: `sᴇᴡᴀ ʙᴏᴛᴢ`, id: `sewa`}},
+    ], 
+     })
+     reply('sukses send bug kepada target')
+}
+handler.help = ['😅 628xxxx']
+
+handler.command = /^(😅)$/i
+handler.owner = true 
+export default handler
